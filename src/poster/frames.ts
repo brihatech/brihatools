b3c6af5966bb61a014d3631cb9e757c249559201
kiran shahi <shahikiran575@gmail.com>
2026-02-11 13:25:13 +0545

    Frame and thumbnail added

--- src/poster/frames.ts
+++ src/poster/frames.ts
@@ -24,7 +24,7 @@ export const FRAMES: FrameConfig[] = [
   {
     id: "frame1",
     src: "/frames/frame1.png",
-    thumbSrc: "/frames/frame1.png",
+    thumbSrc: "/frames/thumbnail1.png",
     categories: ["Chunna Poudel"],
     hasOverlay: false,
     nameText: {
@@ -51,7 +51,7 @@ export const FRAMES: FrameConfig[] = [
   {
     id: "frame2",
     src: "/frames/frame2.png",
-    thumbSrc: "/frames/frame2.png",
+    thumbSrc: "/frames/thumbnail2.png",
     categories: ["Chunna Poudel"],
     hasOverlay: false,
     nameText: {
@@ -78,7 +78,7 @@ export const FRAMES: FrameConfig[] = [
   {
     id: "frame3",
     src: "/frames/frame3.png",
-    thumbSrc: "/frames/frame3.png",
+    thumbSrc: "/frames/thumbnail3.png",
     categories: ["Binod Chaudhary"],
     hasOverlay: false,
     nameText: {
@@ -105,7 +105,7 @@ export const FRAMES: FrameConfig[] = [
   {
     id: "frame4",
     src: "/frames/frame4.png",
-    thumbSrc: "/frames/frame4.png",
+    thumbSrc: "/frames/thumbnail4.png",
     categories: ["Binod Chaudhary"],
     hasOverlay: true,
     overlaySrc: "/frames/frame4_overlay.png",
@@ -133,7 +133,7 @@ export const FRAMES: FrameConfig[] = [
   {
     id: "frame5",
     src: "/frames/frame5.png",
-    thumbSrc: "/frames/frame5.png",
+    thumbSrc: "/frames/thumbnail5.png",
     categories: ["Chunna Poudel"],
     hasOverlay: false,
     nameText: {
@@ -160,7 +160,7 @@ export const FRAMES: FrameConfig[] = [
   {
     id: "frame6",
     src: "/frames/frame6.png",
-    thumbSrc: "/frames/frame6.png",
+    thumbSrc: "/frames/thumbnail6.png",
     categories: ["Chunna Poudel", "Binod Chaudhary"],
     hasOverlay: false,
     nameText: {
@@ -187,7 +187,34 @@ export const FRAMES: FrameConfig[] = [
   {
     id: "frame7",
     src: "/frames/frame7.png",
-    thumbSrc: "/frames/frame7.png",
+    thumbSrc: "/frames/thumbnail7.png",
+    categories: ["Binod Chaudhary"],
+    hasOverlay: false,
+    nameText: {
+      xPct: 62.15,
+      yPct: 85.18,
+      color: "#007400",
+      fontFamily: "Yatra",
+      fontSizePx: 48,
+      fontWeight: "600",
+      scale: 1,
+      backgroundColor: "transparent",
+    },
+    roleText: {
+      xPct: 62.15,
+      yPct: 92.21,
+      color: "#007400",
+      fontFamily: "Vespre",
+      fontSizePx: 36,
+      fontWeight: "500",
+      scale: 1,
+      backgroundColor: "transparent",
+    },
+  },
+  {
+    id: "frame8",
+    src: "/frames/frame8.png",
+    thumbSrc: "/frames/thumbnail8.png",
     categories: ["Binod Chaudhary"],
     hasOverlay: false,
     nameText: {
